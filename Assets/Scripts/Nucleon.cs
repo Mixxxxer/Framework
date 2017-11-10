1d77c85ffd9384792ae2677ebb20e6d17799d7c3
Daniel Nieuwoudt <mixxxxer599@gmail.com>
2017-11-10 20:15:28 +1300

    Rendering Tests

---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+
+    #region Public Members
+
+    public float AttractionForce;
+
+    #endregion
+
+    #region Private Members
+
+    private Rigidbody body;
+
+    #endregion
+
+    #region Public Methods
+
+    public void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        body.AddForce(transform.localPosition * -AttractionForce);
+    }
+
+    #endregion  
+}
---
+++ Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    #region Public Members
+
+    public float MaxNucleons;
+
+    public float SpawnDistance;
+
+    public float TimeBetweenSpawns;
+
+	public Nucleon[] NucleonPrefabs;
+
+    #endregion
+
+    #region Private Members
+
+    private float timeSinceLastSpawn;
+
+    private int currentSpawns = 0;
+
+    #endregion
+
+    #region Public Methods
+
+    public void FixedUpdate()
+    {
+        if (currentSpawns > MaxNucleons)
+            return;
+
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= TimeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= TimeBetweenSpawns;
+
+            SpawnNucleon();
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    public void SpawnNucleon()
+    {
+        var prefab = NucleonPrefabs[Random.Range(0, NucleonPrefabs.Length)];
+        var spawn = Instantiate<Nucleon>(prefab);
+
+        spawn.transform.localPosition = Random.onUnitSphere * SpawnDistance;
+
+        currentSpawns++;
+    }
+
+    #endregion  
+}
