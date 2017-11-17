b36b8f9f58e987263c2d9c94dfc7ec225fcc5d46
Daniel Nieuwoudt <mixxxxer599@gmail.com>
2017-11-18 11:09:14 +1300

    Added buttons

---
+++ Assets/Scripts/UserInterface/GameReadyPanelController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class GameReadyPanelController : MonoBehaviour 
+{
+    
+    #region Caching
+
+    /// <summary>
+    /// Cached copy of the current transform
+    /// </summary>
+    private Transform thisTransform;
+
+    public new Transform transform
+    {
+        get
+        {
+            return thisTransform ?? (thisTransform = base.transform);
+        }
+    }
+
+    #endregion
+
+    #region Public Variables
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #region Initialise 
+
+    public void Awake()
+    {
+    }
+
+    public void Start () 
+    {
+	}
+
+    #endregion
+
+    #region Enable / Disable
+
+    public void OnEnable()
+    {
+    }
+
+    public void OnDisable()
+    {
+    }
+
+    #endregion
+
+    #region Buttons
+
+    public void OnLevelsClick()
+    {
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("OnLevelsClick");
+        }
+    }
+
+    public void OnRateUsClicked()
+    {
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("OnRateUsClicked");
+        }
+    }
+
+    public void OnSoundClicked()
+    {
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("OnSoundClicked");
+        }
+    }
+
+    public void OnLeaderBoardClicked()
+    {
+        if (Debug.isDebugBuild)
+        {
+            Debug.Log("OnLeaderBoardClicked");
+        }
+    }
+
+    #endregion
+}
